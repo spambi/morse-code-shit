da20780d1ce296d4e59682e0e9bed25b706657c5
dickjones <dickjonesbb@protonmail.com>
2022-09-16 23:22:03 +1000

    test stage on static_cast

--- morse-code-shit/morse-code-shit.cpp
+++ morse-code-shit/morse-code-shit.cpp
@@ -5,13 +5,13 @@
 int main()
 {
 	HWAVEOUT hWaveOut = 0;
-	WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 2000, 8000, 1, 8, 0 };
+	WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
 	waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
 	char buffer[8000 * 60] = {};
 
 	// See http://goo.gl/hQdTi
 	for (DWORD t = 0; t < sizeof(buffer); ++t)
-		buffer[t] = static_cast<char>((((t * (t >> 8 | t >> 9) & 46 & t >> 8)) ^ (t & t >> 13 | t >> 6)) & 0xFF);
+		buffer[t] = static_cast<char>((((t * (t >> 8 | t >> 9) & 46 & t >> 8)) ^ (t & t >> 13 | t >> 6)) & 0xF1);
 
 	WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
 	waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
