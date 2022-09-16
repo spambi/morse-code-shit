898d0d9712dac878bb1bddf816a6fae6f836f002
CodyVer <cverner0@outlook.com>
2022-09-16 22:19:07 +0930

    Slutty numbers should be decided by users!

--- morse-code-shit/morse-code-shit.cpp
+++ morse-code-shit/morse-code-shit.cpp
@@ -3,9 +3,14 @@
 
 #include <iostream>
 
+
 int main()
 {
-    std::cout << "YOU ARE GAY AHAHAHAHAHAHAHAHHAHH";
+    std::cout << "YOU ARE GAY AHAHAHAHAHAHAHAHHAHH" << std::endl << "Put in your slutty number: ";
+    int x;
+    std::cin >> x;
+    std::cout << std::endl;
+    std::cout << x;
     return 0;
 }
 
