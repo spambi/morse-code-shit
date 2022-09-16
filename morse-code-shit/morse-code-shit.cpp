2bfa1b7579cf353d096c39a7fa967a0f1dbbc991
dickjones <dickjonesbb@protonmail.com>
2022-09-16 23:10:17 +1000

    test for audio

--- morse-code-shit/morse-code-shit.cpp
+++ morse-code-shit/morse-code-shit.cpp
@@ -1,16 +1,22 @@
-// morse-code-shit.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
+#include <windows.h>
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")
 
 int main()
 {
-    std::cout << "YOU ARE GAY AHAHAHAHAHAHAHAHHAHH" << std::endl << "Put in your slutty number: ";
-    int x;
-    std::cin >> x;
-    std::cout << std::endl;
-    std::cout << x;
-    return 0;
-}
+	HWAVEOUT hWaveOut = 0;
+	WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+	waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+	char buffer[8000 * 60] = {};
 
+	// See http://goo.gl/hQdTi
+	for (DWORD t = 0; t < sizeof(buffer); ++t)
+		buffer[t] = static_cast<char>((((t * (t >> 8 | t >> 9) & 46 & t >> 8)) ^ (t & t >> 13 | t >> 6)) & 0xFF);
+
+	WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+	waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutClose(hWaveOut);
+	Sleep(60 * 1000);
+}
